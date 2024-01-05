9f2162411736472a4a525423a9b83a3218088843
Nourin <nourinkt0098@gmail.com>
2024-01-06 00:00:45 +0530

    update vercel

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -129,7 +129,7 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 
 # Default primary key field type
